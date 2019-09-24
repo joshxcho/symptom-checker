1ab22247900974c93836a0e1e39681bcd85cff0b
joshxcho <joshxcho123@gmail.com>
2019-09-24 02:04:03 -0700

    Add GET request to fetch symptoms data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const config = require('../config');
 
@@ -8,5 +9,28 @@ app.set('port', config.port);
 
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
+
+const readJSONFile = (filename, callback) => {
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    try {
+      callback(null, JSON.parse(data));
+    } catch (exception) {
+      callback(exception);
+    }
+  });
+};
+
+app.get('/api', (req, res) => {
+  readJSONFile('symptoms.json', (err, json) => {
+    if (err) {
+      throw err;
+    }
+    res.send(json);
+  });
+});
 
 app.listen(app.get('port'), () => console.log(`Listening at ${app.get('port')}!`));
