2b076f66ee37de5545bbd07a0608510243fddea1
joshxcho <joshxcho123@gmail.com>
2019-09-24 02:04:45 -0700

    Implement App component and render data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      symptoms: [],
+    };
+  }
+
+  componentDidMount = () => {
+    this.getSymptoms();
+  };
+
+  getSymptoms = () => {
+    axios
+      .get('/api')
+      .then(res => this.setState({
+        symptoms: res.data,
+      }))
+      .catch(err => console.error(err));
+  };
+
   render() {
+    const { symptoms } = this.state;
     return (
-      <h1>
-Hello World!
-      </h1>
+      <div className="landing-page">
+        {symptoms.map(symptom => (
+          <div key={symptom.id}>{symptom.name}</div>
+        ))}
+      </div>
     );
   }
 }
